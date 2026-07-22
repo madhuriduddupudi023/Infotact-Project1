5d223dbde761596421fe92a5ac249dd3e62fb0a5
madhuriduddupudi023 <147141312+madhuriduddupudi023@users.noreply.github.com>
2026-07-22 16:57:24 +0530

    Update validation_queries.sql

--- validation_queries.sql
+++ validation_queries.sql
@@ -1,6 +1,17 @@
+-- Select the AtmoSync database
 USE DATABASE atmosync_db;
+
+-- Select the warehouse schema
 USE SCHEMA warehouse;
+
+-- Verify that the telemetry table exists
 SHOW TABLES;
+
+-- Display the structure of the telemetry table
 DESCRIBE TABLE container_telemetry;
+
+-- View all records in the telemetry table
 SELECT * FROM container_telemetry;
+
+-- Count the total number of records
 SELECT COUNT(*) FROM container_telemetry;
